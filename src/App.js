f69a6895ecd773942efef299f4d77ca131b71bdc
aikene <ikene.aj@gmail.com>
2021-12-08 21:38:50 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import {React, Component} from "react";
 
-function App() {
+class AppA extends Component {
+  render() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>AJ's Sample App</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -20,6 +23,7 @@ function App() {
       </header>
     </div>
   );
+  }
 }
 
-export default App;
+export default AppA;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import AppA from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppA />
   </React.StrictMode>,
   document.getElementById('root')
 );
